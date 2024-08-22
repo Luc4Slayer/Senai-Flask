fec9218cb50cc85e17cc6d2555e0e4f86508e454
Luc4Slayer <luc4ssearom@gmail.com>
2024-08-22 19:21:38 -0300

    Tabela

--- app/models.py
+++ app/models.py
@@ -1,9 +1,22 @@
 from . import db
+from sqlalchemy.orm import backref
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class Estudante(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(120), nullable=False)
-    cpf = db.Column(db.String(15), nullable=False, unique=True)
-
+    Email = db.Column(db.String(20), nullable=False, unique=True)
+    DataNascimento = db.Column(db.Integer(20), nullable=False)
     def __repr__(self):
         return f'<User {self.nome}>'
+
+class Curso (db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False)
+    Duraçao = db.Column(db.String(20), nullable=False, unique=True)
+    Preco = db.Column(db.Integer(20), nullable=False)
+
+class Inscricoes (db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    IdEstudante = db.Column(db.Integer,db.ForieingKey(Estudante(Id)), nullable=False)
+    IdCurso = db.Column(db.Integer,db.ForieingKey(Curso(Id)), nullable=False, unique=True)
+    DataInscricao = db.Column(db.Integer(20), nullable=False)
