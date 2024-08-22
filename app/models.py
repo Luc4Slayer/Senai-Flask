1d9f7421aaf1a8c62bccd14f7c848687b84177e8
Luc4Slayer <luc4ssearom@gmail.com>
2024-08-22 20:58:57 -0300

    Tabela

--- app/models.py
+++ app/models.py
@@ -2,21 +2,21 @@ from . import db
 from sqlalchemy.orm import backref
 
 class Estudante(db.Model):
-    Id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(120), nullable=False)
-    Email = db.Column(db.String(20), nullable=False, unique=True)
-    DataNascimento = db.Column(db.Integer(20), nullable=False)
+    email = db.Column(db.String(20), nullable=False, unique=True)
+    data_nascimento = db.Column(db.DateTime, nullable=False)
     def __repr__(self):
         return f'<User {self.nome}>'
 
 class Curso (db.Model):
-    Id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(120), nullable=False)
-    Duraçao = db.Column(db.String(20), nullable=False, unique=True)
-    Preco = db.Column(db.Integer(20), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    nonlocalome = db.Column(db.String(120), nullable=False)
+    duraçao = db.Column(db.String(20), nullable=False, unique=True)
+    preco = db.Column(db.Integer(20), nullable=False)
 
 class Inscricoes (db.Model):
-    Id = db.Column(db.Integer, primary_key=True)
-    IdEstudante = db.Column(db.Integer,db.ForieingKey(Estudante(Id)), nullable=False)
-    IdCurso = db.Column(db.Integer,db.ForieingKey(Curso(Id)), nullable=False, unique=True)
-    DataInscricao = db.Column(db.Integer(20), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    idEstudante = db.Column(db.Integer,db.ForeingKey('estudante.id'))
+    idCurso = db.Column(db.Integer,db.ForeingKey('curso.id'))
+    data_inscricao = db.Column(db.DateTime, nullable=False)
